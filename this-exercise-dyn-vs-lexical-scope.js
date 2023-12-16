e540fe6126fb2a9aaf14b2f22f7ffb1438b18322
Jason Bariamis <jasonbari646@gmail.com>
2023-12-16 21:06:43 +0200

    complete exercise

--- this-exercise-dyn-vs-lexical-scope.js
+++ this-exercise-dyn-vs-lexical-scope.js
@@ -24,4 +24,34 @@ const obj = {
     }
 }
 
-// this in b will be window - one of the biggest gotchas when it comes to this keyword, because the thiskeyword is not lexical scoped. That is, it doesn't matter where it is written, it matters how the function was called.
+// this in b will be window - one of the biggest gotchas when it comes to this keyword, because the this keyword is not lexical scoped. That is, it doesn't matter where it is written, it matters how the function was called.
+
+// the THIS keyword is actually dynamically scoped. That is, it doesn't matter where its written, it matters how the function was called.
+// how do we avoid this pitfall?
+// by using ARROW FUNCTIONS - the are lexically bound, that is they have a lexical THIS behavior unlike normal functions
+
+
+const obj2 = {
+    name: 'matt',
+    sing() 
+    {
+        console.log('a', this);
+        var anotherFunc = () => {
+            console.log('b', this);
+        }
+        anotherFunc();
+    }
+}
+
+// before arrow functions in ES6 we used 'bind' to this to fix this issue
+const obj3 = {
+    name: 'matt',
+    sing() 
+    {
+        console.log('a', this);
+        var anotherFunc = () => {
+            console.log('b', this);
+        }
+        return anotherFunc.bind(this);
+    }
+}
